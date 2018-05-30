81aad69e86c1ecd17a1857c6a6db09a9e1c250fa
quoccuong <tran.cuongquoc@gmail.com>
2018-05-30 14:59:07 +0700

    Almost done (no saving method used)

---
+++ Foodtracker(28052018)/Meal.swift
@@ -0,0 +1,25 @@
+//
+//  Meal.swift
+//  Foodtracker(28052018)
+//
+//  Created by quoccuong on 2018/05/29.
+//  Copyright © 2018 quoccuong. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class Meal {
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+
+    init?(name: String, photo: UIImage?, rating: Int) {
+        guard !name.isEmpty else { return nil }
+        guard (rating > 0) && (rating <= 5) else { return nil }
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
---
+++ Foodtracker(28052018)/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  Foodtracker(28052018)
+//
+//  Created by quoccuong on 2018/05/29.
+//  Copyright © 2018 quoccuong. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
